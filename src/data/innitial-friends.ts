3bbf8532536aed0485f9bc56a88020dcf6aae1d1
Ana Diru <ajd6735@psu.edu>
2025-04-01 10:34:45 -0400

    innitial commit

---
+++ src/data/innitial-friends.ts
@@ -0,0 +1,22 @@
+const innitialFriends = [
+    {
+        id: 113456,
+        name: "Clark",
+        image: "",
+        balance: -7
+    },
+    {
+        id: 113457,
+        name: "Maria",
+        image: "",
+        balance: 30
+    },
+    {
+        id: 113458,
+        name: "Jason",
+        image: "",
+        balance: 0
+    }
+];
+
+export default innitialFriends;
